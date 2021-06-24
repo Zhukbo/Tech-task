4554f6c8ffdbdbf3a787f502454946066082a378
Bogdan Zhuk <bogdan_zhyk@ukr.net>
2021-06-24 19:15:50 +0300

    Added task 5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(ShowCurrentTemp("Kyiv"));
+                Console.WriteLine(ShowCurrentTemp("Odesa"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        class Weather
+        {
+            public string Temperature { get; set; }
+        }
+        static string ShowCurrentTemp (string city)
+        {
+
+            string response = "";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://goweather.herokuapp.com/weather/" + city);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            Weather temperature = JsonConvert.DeserializeObject<Weather>(response);
+            return temperature.Temperature;
+        }
+
+    }
+}
